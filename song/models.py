f38ddb0d796005da297450cb31c7c1c8e328651f
Hadiseh Pourali <h.pourali.a@gmail.com>
2022-05-24 17:38:54 +0430

    Artist model added

---
+++ song/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=250)
+    country = models.CharField(max_length=150)
+    birth_year = models.IntegerField()
+    genre = models.CharField(max_length=150)
+
+
+class Song(models.Model):
+    Title = models.CharField(max_length=250)
+    release_date = models.IntegerField()
+    length = models.DateField()
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+
+
